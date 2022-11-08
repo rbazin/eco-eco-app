e15d0a622a8d273f3438d1d0985f47aaa8d167bc
Romain Bazin <romain.bzin@gmail.com>
2022-11-08 12:58:52 -0500

    initial commit

---
+++ frontend/src/components/WelcomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="welcome-page">
+    <h1 class="title is-1">Welcome to the Eco-Eco App</h1>
+    <p class="subtitle is-3">This is the welcome page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WelcomePage",
+};
+</script>
+<style></style>
---
+++ frontend/src/routes.js
@@ -0,0 +1,4 @@
+// Path: frontend\src\routes.js
+import WelcomePage from "./components/WelcomePage.vue";
+
+export const routes = [{ path: "/", component: WelcomePage }];
