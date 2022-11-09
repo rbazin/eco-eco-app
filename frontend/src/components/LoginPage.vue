14994757aae4c5972ff708f611ac83ffbbb0082d
Romain Bazin <romain.bzin@gmail.com>
2022-11-09 13:55:03 -0500

    login

--- frontend/src/components/LoginPage.vue
+++ frontend/src/components/LoginPage.vue
@@ -1,17 +1,21 @@
 <template>
   <div class="login-page is-fullheight">
-    <div class="columns is-mobile is-centered">
-      <div class="column is-6">
-        <img src="../assets/set_1/1.png" alt="Eco-Eco Logo" width="275" />
-      </div>
-    </div>
+    <figure class="content has-text-centered mb-0">
+      <img src="../assets/set_1/1.png" alt="Eco-Eco Logo" width="200" />
+    </figure>
 
     <h1 class="title is-1 has-text-centered">Login</h1>
     <p class="subtitle has-text-centered is-3">Please login to continue</p>
     <form @submit.prevent="login">
-      <div class="field">
-        <label class="label">Username</label>
-        <div class="control">
+      <div class="field columns is-mobile px-5">
+        <figure class="column is-3 has-text-centered">
+          <img
+            src="../assets/set_1/u.png"
+            alt="User login icon"
+            class="image is-48x48"
+          />
+        </figure>
+        <div class="control column">
           <input
             class="input"
             type="text"
@@ -20,9 +24,15 @@
           />
         </div>
       </div>
-      <div class="field">
-        <label class="label">Password</label>
-        <div class="control">
+      <div class="field columns is-mobile px-5">
+        <figure class="column is-3 has-text-centered">
+          <img
+            src="../assets/set_1/9.png"
+            alt="User login icon"
+            class="image is-48x48"
+          />
+        </figure>
+        <div class="control column">
           <input
             class="input"
             type="password"
@@ -32,16 +42,18 @@
         </div>
       </div>
       <div class="field">
-        <div class="control">
+        <div class="control has-text-centered">
           <button class="button is-primary">Login</button>
         </div>
       </div>
     </form>
     <img class="is-image" src="../assets/set_1/divider.png" />
-    <p class="subtitle has-text-centered is-3">Don't have an account yet ?</p>
-    <router-link to="/register" class="button is-primary">
-      Register
-    </router-link>
+    <p class="subtitle has-text-centered is-4">Don't have an account yet ?</p>
+    <div class="has-text-centered">
+      <router-link to="/register" class="button is-primary">
+        Register
+      </router-link>
+    </div>
     <img class="is-image" src="../assets/set_1/8.png" />
   </div>
 </template>
@@ -56,12 +68,7 @@ export default {
     };
   },
   methods: {
-    login() {
-      this.$store.dispatch("login", {
-        username: this.username,
-        password: this.password,
-      });
-    },
+    login() {},
   },
 };
 </script>
--- frontend/src/components/WelcomePage.vue
+++ frontend/src/components/WelcomePage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="welcome-page">
+  <div class="welcome-page content">
     <!-- Just a simple test page, for now it's the landing page -->
     <h1 class="title is-1">Welcome to the Eco-Eco App</h1>
     <p class="subtitle is-3">This is the welcome page</p>
-    <router-link to="/login" class="button is-primary">Login</router-link>
+    <div class="columns">
+      <router-link to="/login" class="button is-primary column"
+        >Login</router-link
+      >
+    </div>
   </div>
 </template>
 
