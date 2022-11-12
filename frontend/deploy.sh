9a98fc982d529b442426aac3f3b79bc059b0834d
Romain Bazin <romain.bzin@gmail.com>
2022-11-11 23:32:44 -0500

    deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-# abort on errors
+
 set -e
-# build
+
+# Build the frontend
 yarn build
+
 # navigate into the build output directory
 cd dist
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
+
 git push -f git@github.com:rbazin/eco-eco-app.git main:gh-pages
+
 cd -
