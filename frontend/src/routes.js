e68abe5731fcbb742d938d79ccb825b7a0f38ff2
Romain Bazin <romain.bzin@gmail.com>
2022-11-12 14:58:46 -0500

    updating routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,10 @@
 // Path: frontend\src\routes.js
-import WelcomePage from "./components/WelcomePage.vue";
 import LoginPage from "./components/LoginPage.vue";
 import RegisterPage from "./components/RegisterPage.vue";
 import HomePage from "./components/HomePage.vue";
 
 export const routes = [
-  { path: "/", component: WelcomePage },
-  { path: "/login", component: LoginPage },
+  { path: "/", component: LoginPage },
   { path: "/register", component: RegisterPage },
   { path: "/home", component: HomePage },
 ];
