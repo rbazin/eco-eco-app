9e4638336528851e47f51e1cbc68b5cffc1946d9
Romain Bazin <romain.bzin@gmail.com>
2022-11-12 00:19:40 -0500

    deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:rbazin/eco-eco-app.git main:gh-pages
+git push -f git@github.com:rbazin/eco-eco-app/frontend.git main:gh-pages
 
 cd -
