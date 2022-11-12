a5a67abd6a27af1f0ed42b26f8d46385c79a2489
Romain Bazin <romain.bzin@gmail.com>
2022-11-12 00:10:15 -0500

    deploy

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env sh
+
 # abort on errors
 set -e
+
 # build
-yarn run build
+yarn build
+
 # navigate into the build output directory
 cd dist
+
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
+
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/hello-world.git master:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f git@github.com:rbazin/eco-eco-app.git main:gh-pages
+
 cd -
