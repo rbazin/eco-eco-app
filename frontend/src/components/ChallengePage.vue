021310ccec28343a97eb53897de9c47068e2cf74
Romain Bazin <romain.bzin@gmail.com>
2022-11-17 22:17:20 -0500

    call backend for challenges

--- frontend/src/components/ChallengePage.vue
+++ frontend/src/components/ChallengePage.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="container">
-    <h1 class="is-title is-size-1">Challenge page</h1>
+    <!-- Back button -->
+    <div @click="goBack" class="px-5 py-5">
+      <font-awesome-icon class="is-size-2" icon="fa-solid fa-arrow-left" />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "ChallengePage",
+  data() {
+    return {
+      challenges: {},
+    };
+  },
+  methods: {
+    goBack() {
+      this.$router.push("/home");
+    },
+    // Function to get the challenge from the backend
+    getChallenges() {
+      // Get 3 challenges from the backend, each challenge should be an object {id, title, ecoFact}
+      axios
+        .get("http://localhost:5000/api/challenges")
+        .then((response) => {
+          console.log(response);
+          if (response.data.success) {
+            this.challenges = response.data.challenges;
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
 
