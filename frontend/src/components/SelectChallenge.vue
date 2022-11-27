8ca2b75bc02570bba95ad3bf21617f99ca22be73
Romain Bazin <romain.bzin@gmail.com>
2022-11-27 12:18:28 -0500

    select challenge from all challenges list

--- frontend/src/components/SelectChallenge.vue
+++ frontend/src/components/SelectChallenge.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <!-- Back button -->
     <div @click="goBack" class="px-5 py-5 mb-5">
-      <font-awesome-icon class="is-size-2" icon="fa-solid fa-arrow-left" />
+      <font-awesome-icon class="is-size-2" icon="fa-solid fa-arrow-left"/>
     </div>
 
     <!-- Challenge info -->
@@ -11,7 +11,7 @@
       <div class="is-size-2">
         <span class="icon-text">
           <span class="icon"
-            ><font-awesome-icon
+          ><font-awesome-icon
               color="rgb(93, 173, 236)"
               icon="fa-solid fa-droplet"
           /></span>
@@ -26,9 +26,9 @@
     <div class="field px-5">
       <div class="control has-text-centered">
         <button
-          @click="acceptChallenge"
-          class="button is-primary"
-          type="button"
+            @click="acceptChallenge"
+            class="button is-primary"
+            type="button"
         >
           Accept challenge
         </button>
@@ -38,8 +38,8 @@
 </template>
 
 <script>
-import { challengeStore } from "../stores/challengeStore";
-import { userStore } from "../stores/userStore";
+import {challengeStore} from "../stores/challengeStore";
+import {userStore} from "../stores/userStore";
 import axios from "axios";
 
 export default {
@@ -63,9 +63,17 @@ export default {
     };
   },
   mounted() {
-    this.challenge = this.challenge_store.challenges.find(
-      (challenge) => challenge.id == this.$route.params.id
+    const challenge = this.challenge_store.challenges.find(
+        (challenge) => challenge.id == this.$route.params.id
     );
+    if (challenge == undefined) {
+      this.challenge = this.challenge_store.allChallenges.find(
+          (challenge) => challenge.id == this.$route.params.id
+      );
+
+    } else {
+      this.challenge = challenge;
+    }
   },
   methods: {
     goBack() {
@@ -74,20 +82,20 @@ export default {
     acceptChallenge() {
       // send the challenge to the backend
       axios
-        .post("http://localhost:5000/api/accept_challenge", {
-          userId: this.user_store.userId,
-          challengeId: this.challenge.id,
-          isAccepted: true,
-        })
-        .then((response) => {
-          if (response.data.success) {
-            this.user_store.userActiveChallenge = this.challenge;
-            this.$router.push("/home");
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          .post("http://localhost:5000/api/accept_challenge", {
+            userId: this.user_store.userId,
+            challengeId: this.challenge.id,
+            isAccepted: true,
+          })
+          .then((response) => {
+            if (response.data.success) {
+              this.user_store.userActiveChallenge = this.challenge;
+              this.$router.push("/home");
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
     },
   },
 };
@@ -98,13 +106,16 @@ export default {
   min-height: 100vh;
   background-image: url("../assets/set_1/login_background.png");
 }
+
 .fact {
   color: #055c52;
 }
+
 .title {
   color: #066284;
   text-shadow: 4 2 #1199ee;
 }
+
 .subtitle {
   color: #22aa99;
 }
