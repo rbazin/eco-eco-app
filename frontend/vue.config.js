35739748952abf2e9577d9b95d98dda4eabb5d04
Romain Bazin <romain.bzin@gmail.com>
2022-12-01 23:30:07 -0500

    changed public path to relative path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,5 @@
 // To correctly export the frontend on github pages
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/eco-eco-app/'
-    : '/'
-
+  publicPath : "./"
 }
